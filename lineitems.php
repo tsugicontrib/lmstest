1a4d3891059db987deee240afce06834ea460207
Charles Severance <csev@umich.edu>
2019-05-09 07:09:56 -0400

    Yay for deleting lines.

--- lineitems.php
+++ lineitems.php
@@ -18,22 +18,6 @@ $OUTPUT->topNav();
 
 require_once("nav.php");
 
-$lti13_token_url = LTIX::ltiParameter('lti13_token_url');
-$lti13_privkey = LTIX::decrypt_secret(LTIX::ltiParameter('lti13_privkey'));
-$lti13_lineitems = LTIX::ltiParameter('lti13_lineitems');
-$lti13_client_id = LTIX::ltiParameter('lti13_client_id');
-
-$missing = '';
-if ( strlen($lti13_client_id) < 1 ) $missing .= ' ' . 'client_id';
-if ( strlen($lti13_privkey) < 1 ) $missing .= ' ' . 'private_key';
-if ( strlen($lti13_token_url) < 1 ) $missing .= ' ' . 'token_url';
-
-if ( strlen($missing) > 0 ) {
-    echo('Missing '.$missing);
-    $OUTPUT->footer();
-    return;
-}
-
 ?>
 <div id="iframe-dialog" title="Read Only Dialog" style="display: none;">
    <img src="<?= $OUTPUT->getSpinnerUrl() ?>" id="iframe-spinner"><br/>
@@ -43,56 +27,34 @@ if ( strlen($missing) > 0 ) {
 </div>
 <div id="tabs">
   <ul>
-    <li><a href="#tabs-1">Token</a></li>
-    <li><a href="#tabs-1d">Debug Log</a></li>
     <li><a href="#tabs-2">LineItems List</a></li>
     <li><a href="#tabs-2d">Debug Log</a></li>
   </ul>
-  <div id="tabs-1">
-    <pre>
-<?php
-$lineitems_access_token = false;
-$debug_log = array();
-if ( strlen($lti13_lineitems) > 0 && strlen($lti13_lineitems) > 0 ) {
-    echo("Token URL: ".$lti13_token_url."\n");
-    $lineitems_access_token = LTI13::getLineItemsToken($CFG->wwwroot, $lti13_client_id, $lti13_token_url, $lti13_privkey, $debug_log);
-    echo("LineItems Access Token=". $lineitems_access_token . "\n");
-} else {
-    echo("Did not receive lineitems url\n");
-}
-?>
-    </pre>
-  </div>
-<?php print_debug_log("tabs-1d", $debug_log); ?>
   <div id="tabs-2">
     <pre>
 <?php
 $debug_log = array();
-if ( $lineitems_access_token ) {
-    echo("LineItems URL: ".$lti13_lineitems."\n");
-    $lineitems = LTI13::loadLineItems($lti13_lineitems, $lineitems_access_token, $debug_log);
-    if ( is_string($lineitems) ) {
-        echo($lineitems."\n");
-    } else {
-        echo("Loaded ".count($lineitems)." members\n");
-        echo("\nAll lineitems:\n");
-        echo(htmlentities(Output::safe_print_r($lineitems)));
-        if ( count($lineitems) > 0 && isset($lineitems[0]->id) && is_string($lineitems[0]->id) ) {
-            $lineitem_url = $lineitems[0]->id;
-            echo("Loading data for the first lineitem...\n");
-            $lineitem = LTI13::loadLineItem($lineitem_url, $lineitems_access_token, $debug_log);
-            if ( is_string($lineitem) ) {
-                echo("Failed loading ".htmlentities($lineitem_url)."\n");
-                echo("Status: ".$lineitem."\n");
-            } else {
-                echo(htmlentities(Output::safe_print_r($lineitem)));
-            }
-        } else {
-            echo("Did not find valid lineitem to test single item GET...\n");
-        }
-    }
+$search = false;
+$lineitems = $LTI->context->loadLineItems($search, $debug_log);
+if ( is_string($lineitems) ) {
+    echo($lineitems."\n");
 } else {
-    echo("Did not get lineitems_access_token\n");
+    echo("Loaded ".count($lineitems)." members\n");
+    echo("\nAll lineitems:\n");
+    echo(htmlentities(Output::safe_print_r($lineitems)));
+    if ( count($lineitems) > 0 && isset($lineitems[0]->id) && is_string($lineitems[0]->id) ) {
+        $lineitem_url = $lineitems[0]->id;
+        echo("Loading data for the first lineitem...\n");
+        $lineitem = $LTI->context->loadLineItem($lineitem_url, $debug_log);
+        if ( is_string($lineitem) ) {
+            echo("Failed loading ".htmlentities($lineitem_url)."\n");
+            echo("Status: ".$lineitem."\n");
+        } else {
+            echo(htmlentities(Output::safe_print_r($lineitem)));
+        }
+    } else {
+        echo("Did not find valid lineitem to test single item GET...\n");
+    }
 }
 ?>
     </pre>
